3e106072b0e3603afa54b98b9b598fb213a6236f
Sohel Mansuri <sohelm786@gmail.com>
2020-09-10 21:34:40 -0500

    Added animations to each section.

--- js/script.js
+++ js/script.js
@@ -18,4 +18,51 @@ $(document).ready(function() {
             menuIcon.setAttribute("name", "menu-outline");
         }  
     });
+    
+    /* Animations on scroll */
+    /* Flags Animation */
+    var flagsAnimation = new Waypoint({
+      element: document.getElementById('js--wp-1'),
+      handler: function(direction) {
+        var pulseAnimation = " animated animate__animated animate__pulse";
+          
+        var iFI = document.getElementById("animate-indian-flag");
+        var aFI = document.getElementById("animate-arabic-flag");
+        var pFI = document.getElementById("animate-pakistani-flag");
+        var tFI = document.getElementById("animate-turkish-flag");
+          
+        iFI.className += pulseAnimation;
+        aFI.className += pulseAnimation;
+        pFI.className += pulseAnimation;
+        tFI.className += pulseAnimation;
+      }});
+    
+    /* Customer Reviews Animation */
+    var customerReviewsAnimation = new Waypoint({
+      element: document.getElementById('js--wp-2'),
+      handler: function(direction) {
+        var bounceInDown = " animated animate__animated animate__bounceInDown";
+        var x = document.getElementsByClassName("customer-review-box");
+        var i;
+        for (i = 0; i < x.length; i++) {
+          x[i].className += bounceInDown;
+        }
+      }
+    });
+    
+     /* Newsletter Animation */
+    var newsletterAnimation = new Waypoint({
+      element: document.getElementById('js--wp-3'),
+      handler: function(direction) {
+        var bounceInDown = " animated animate__animated animate__lightSpeedInRight";
+        var x = document.getElementsByClassName("newsletter-image-holder");
+        var i;
+        for (i = 0; i < x.length; i++) {
+          x[i].className += bounceInDown;
+        }
+      }, offset: '400px'
+    });
+    
+//    , offset: '250px' 
+//    , offset: '400px' 
 });
